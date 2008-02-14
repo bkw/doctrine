02524841efbb3a10c8bb5130e9e96995356e4d2d
adrive <adrive@625475ce-881a-0410-a577-b389adb331d8>
2008-02-14 13:10:21 +0000

    Merge r3769 (fixed problem with convertBoolean in Pgsql)
    git-svn-id: http://svn.doctrine-project.org/branches/0.10@3770 625475ce-881a-0410-a577-b389adb331d8

--- tests/Export/PgsqlTestCase.php
+++ tests/Export/PgsqlTestCase.php
@@ -104,13 +104,15 @@ class Doctrine_Export_Pgsql_TestCase extends Doctrine_UnitTestCase
     {
         $name = 'mytable';
         $fields  = array('name' => array('type' => 'char', 'length' => 10, 'default' => 'def'),
-                         'type' => array('type' => 'integer', 'length' => 3, 'default' => 12)
+                         'type' => array('type' => 'integer', 'length' => 3, 'default' => 12),
+                         'is_active' => array('type' => 'boolean', 'default'=>'0'),
+                         'is_admin'  => array('type' => 'boolean', 'default'=>'true'),
                          );
                          
         $options = array('primary' => array('name', 'type'));
         $this->export->createTable($name, $fields, $options);
 
-        $this->assertEqual($this->adapter->pop(), 'CREATE TABLE mytable (name CHAR(10) DEFAULT \'def\', type INT DEFAULT 12, PRIMARY KEY(name, type))');
+        $this->assertEqual($this->adapter->pop(), 'CREATE TABLE mytable (name CHAR(10) DEFAULT \'def\', type INT DEFAULT 12, is_active BOOLEAN DEFAULT false, is_admin BOOLEAN DEFAULT true, PRIMARY KEY(name, type))');
     }
     public function testCreateTableSupportsMultiplePks()
      {
