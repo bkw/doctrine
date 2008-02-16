f0a2d3853ab425505dc797850030a2eeaa792c0d
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-02-16 04:31:16 +0000

    Updated package information for 0.9.1 and 0.10.1 releases.
    git-svn-id: http://svn.doctrine-project.org/tags/0.9.1@3804 625475ce-881a-0410-a577-b389adb331d8

--- lib/Doctrine.php
+++ lib/Doctrine.php
@@ -36,7 +36,7 @@ final class Doctrine
     /**
      * VERSION
      */
-    const VERSION                   = '0.9.0';
+    const VERSION                   = '0.9.1';
     
     /**
      * ERROR CONSTANTS
