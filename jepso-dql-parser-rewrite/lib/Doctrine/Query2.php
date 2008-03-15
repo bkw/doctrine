6e7ccd99aa1d0c6adbe8f503331939c20c7dda6c
guilhermeblanco <guilhermeblanco@625475ce-881a-0410-a577-b389adb331d8>
2008-03-15 22:20:46 +0000

    Fixed Doctrine_Pager::getResultsInPage() as reported by Matt Marcum through email.
    git-svn-id: http://svn.doctrine-project.org/branches/experimental@3991 625475ce-881a-0410-a577-b389adb331d8

--- jepso-dql-parser-rewrite/lib/Doctrine/Query2.php
+++ jepso-dql-parser-rewrite/lib/Doctrine/Query2.php
@@ -20,7 +20,7 @@
  * <http://www.phpdoctrine.org>.
  */
 
-Doctrine::autoload('Doctrine_Query_Abstract');
+Doctrine::autoload('Doctrine_Query_Abstract2');
 
 /**
  * Doctrine_Query2
@@ -61,11 +61,331 @@ Doctrine::autoload('Doctrine_Query_Abstract');
  */
 class Doctrine_Query2 extends Doctrine_Query_Abstract2
 {
+    /**
+     * @var Doctrine_Connection The connection used by this query object.
+     */
+    protected $_connection;
 
-    public function __construct()
+    /**
+     * @var Doctrine_Hydrator   The hydrator object used to hydrate query results.
+     */
+    protected $_hydrator;
+
+    /**
+     * @var Doctrine_Query_Parser  The parser that is used during the DQL convertion to SQL.
+     */
+    protected $_parser;
+
+
+    // Caching Stuff
+
+    /**
+     * @var Doctrine_Cache_Interface  The cache driver used for caching result sets.
+     */
+    protected $_resultCache;
+
+    /**
+     * @var boolean Boolean value that indicates whether or not expire the result cache.
+     */
+    protected $_expireResultCache = false;
+
+    /**
+     * @var int Result Cache lifetime.
+     */
+    protected $_resultCacheTTL;
+
+
+    /**
+     * @var Doctrine_Cache_Interface  The cache driver used for caching queries.
+     */
+    protected $_queryCache;
+
+    /**
+     * @var boolean Boolean value that indicates whether or not expire the query cache.
+     */
+    protected $_expireQueryCache = false;
+
+    /**
+     * @var int Query Cache lifetime.
+     */
+    protected $_queryCacheTTL;
+
+    // End of Caching Stuff
+
+
+    public function __construct(Doctrine_Connection $conn = null, Doctrine_Hydrator_Abstract $hydrator = null)
     {
+        $this->setConnection($conn);
+
+        if ($hydrator === null) {
+            $hydrator = new Doctrine_Hydrator();
+        }
+
+        $this->_parser = new Doctrine_Query_Parser($this);
+        $this->_hydrator = $hydrator;
+
         $this->free();
     }
+
+
+    /**
+     * create
+     *
+     * Returns a new Doctrine_Query object
+     *
+     * @param Doctrine_Connection $conn     optional connection parameter
+     * @return Doctrine_Query
+     */
+    public static function create($conn = null)
+    {
+        return new self($conn);
+    }
+
+
+    /**
+     * getConnection
+     *
+     * Retrieves the assocated Doctrine_Connection to this Doctrine_Query
+     *
+     * @return Doctrine_Connection
+     */
+    public function getConnection()
+    {
+        return $this->_connection;
+    }
+
+
+    /**
+     * setConnection
+     *
+     * Defines an assocated Doctrine_Connection to this Doctrine_Query
+     *
+     * @param Doctrine_Connection $conn A valid Doctrine_Connection
+     * @return void
+     */
+    public function setConnection(Doctrine_Connection $conn = null)
+    {
+        if ($conn === null) {
+            $conn = Doctrine_Manager::getInstance()->getCurrentConnection();
+        }
+
+        $this->_connection = $conn;
+    }
+
+
+    /**
+     * getHydrator
+     *
+     * Returns the hydrator associated with this query object
+     *
+     * @return Doctrine_Hydrator The hydrator associated with this query object
+     */
+    public function getHydrator()
+    {
+        return $this->_hydrator;
+    }
+
+
+    /**
+     * getParser
+     *
+     * Returns the parser associated with this query object
+     *
+     * @return Doctrine_Query_Parser The parser associated with this query object
+     */
+    public function getParser()
+    {
+        return $this->_parser;
+    }
+
+
+    /**
+     * setResultCache
+     *
+     * Defines a cache driver to be used for caching result sets.
+     *
+     * @param Doctrine_Cache_Interface|null $driver Cache driver
+     * @return Doctrine_Query
+     */
+    public function setResultCache($resultCache)
+    {
+        if ($resultCache !== null && ! ($resultCache instanceof Doctrine_Cache_Interface)) {
+            throw new Doctrine_Query_Exception(
+                'Method setResultCache() accepts only an instance of Doctrine_Cache_Interface or null.'
+            );
+        }
+
+        $this->_resultCache = $resultCache;
+
+        return $this;
+    }
+
+
+    /**
+     * getResultCache
+     *
+     * Returns the cache driver used for caching result sets.
+     *
+     * @return Doctrine_Cache_Interface Cache driver
+     */
+    public function getResultCache()
+    {
+        if ($this->_resultCache instanceof Doctrine_Cache_Interface) {
+            return $this->_resultCache;
+        } else {
+            return $this->_connection->getResultCacheDriver();
+        }
+    }
+
+
+    /**
+     * setResultCacheLifetime
+     *
+     * Defines how long the result cache will be active before expire.
+     *
+     * @param integer $timeToLive How long the cache entry is valid
+     * @return Doctrine_Query
+     */
+    public function setResultCacheLifetime($timeToLive)
+    {
+        if ($timeToLive !== null) {
+            $timeToLive = (int) $timeToLive;
+        }
+
+        $this->_resultCacheTTL = $timeToLive;
+
+        return $this;
+    }
+
+
+    public function getResultCacheLifetime()
+    {
+        return $this->_resultCacheTTL;
+    }
+
+
+    /**
+     * setExpireResultCache
+     *
+     * Defines if the resultset cache is active or not.
+     *
+     * @param boolean $expire Whether or not to force resultset cache expiration.
+     * @return Doctrine_Query
+     */
+    public function setExpireResultCache($expire = true)
+    {
+        $this->_expireResultCache = (bool) $expire;
+
+        return $this;
+    }
+
+
+    public function getExpireResultCache()
+    {
+        return $this->_expireResultCache;
+    }
+
+
+    /**
+     * setQueryCache
+     *
+     * Defines a cache driver to be used for caching queries.
+     *
+     * @param Doctrine_Cache_Interface|null $driver Cache driver
+     * @return Doctrine_Query
+     */
+    public function setQueryCache($queryCache)
+    {
+        if ($queryCache !== null && ! ($queryCache instanceof Doctrine_Cache_Interface)) {
+            throw new Doctrine_Query_Exception(
+                'Method setResultCache() accepts only an instance of Doctrine_Cache_Interface or null.'
+            );
+        }
+
+        $this->_queryCache = $queryCache;
+
+        return $this;
+    }
+
+
+    /**
+     * getQueryCache
+     *
+     * Returns the cache driver used for caching queries.
+     *
+     * @return Doctrine_Cache_Interface Cache driver
+     */
+    public function getQueryCache()
+    {
+        if ($this->_queryCache instanceof Doctrine_Cache_Interface) {
+            return $this->_queryCache;
+        } else {
+            return $this->_connection->getQueryCacheDriver();
+        }
+    }
+
+
+    /**
+     * setQueryCacheLifetime
+     *
+     * Defines how long the query cache will be active before expire.
+     *
+     * @param integer $timeToLive How long the cache entry is valid
+     * @return Doctrine_Query
+     */
+    public function setQueryCacheLifetime($timeToLive)
+    {
+        if ($timeToLive !== null) {
+            $timeToLive = (int) $timeToLive;
+        }
+
+        $this->_queryCacheTTL = $timeToLive;
+
+        return $this;
+    }
+
+
+    public function getQueryCacheLifetime()
+    {
+        return $this->_queryCacheTTL;
+    }
+
+
+    /**
+     * setExpireQueryCache
+     *
+     * Defines if the query cache is active or not.
+     *
+     * @param boolean $expire Whether or not to force query cache expiration.
+     * @return Doctrine_Query
+     */
+    public function setExpireQueryCache($expire = true)
+    {
+        $this->_expireQueryCache = (bool) $expire;
+
+        return $this;
+    }
+
+
+    public function getExpireQueryCache()
+    {
+        return $this->_expireQueryCache;
+    }
+
+
+    /**
+     * setHydrationMode
+     *
+     * @params $hydrationMode Doctrine processing mode to be used during hydration process.
+     * @return Doctrine_Query
+     */
+    public function setHydrationMode($hydrationMode)
+    {
+        $this->_hydrator->setHydrationMode($hydrationMode);
+
+        return $this;
+    }
+
 
 
     public function getSqlQuery($params = array())
