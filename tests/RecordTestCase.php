c88e57c0f9e413be7521f82e1d6a036963dd5d6c
romanb <romanb@625475ce-881a-0410-a577-b389adb331d8>
2008-03-19 18:24:15 +0000

    added xslt stylesheet to generate changelogs with svn log
    git-svn-id: http://svn.doctrine-project.org/branches/0.10@4054 625475ce-881a-0410-a577-b389adb331d8

--- tests/RecordTestCase.php
+++ tests/RecordTestCase.php
@@ -905,8 +905,8 @@ class Doctrine_Record_TestCase extends Doctrine_UnitTestCase
         }
     }
     
-    /*
-    public function testFirstCharUnderscoreColumn()
+    
+    /*public function testFirstCharUnderscoreInColumnNameAndTableName()
     {
         $record = new UnderscoreColumn();
         $record->_underscore_ = 'test';
@@ -920,6 +920,6 @@ class Doctrine_Record_TestCase extends Doctrine_UnitTestCase
         
         $result = $query->execute()->getFirst();
         $this->assertEqual($result->_underscore_, 'test');
-    }
-    */
+    }*/
+    
 }
