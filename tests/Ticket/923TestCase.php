e863bd33d0534aeed27358da4dc382d95992eb29
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-03-28 19:40:28 +0000

    Updated test.
    git-svn-id: http://svn.doctrine-project.org/branches/0.10@4153 625475ce-881a-0410-a577-b389adb331d8

--- tests/Ticket/923TestCase.php
+++ tests/Ticket/923TestCase.php
@@ -34,30 +34,29 @@ class Doctrine_Ticket_923_TestCase extends Doctrine_UnitTestCase {
 
     public function prepareData() 
     {
-      $d = new T923_Diagnostic();
-      $d->id_type = 101;
-      $d->id = 26;
-      $d->diagnostic_id = 75444;
-      $d->diag_timestamp = '2008-03-27 12:00:00';
-      $d->operator_id = 1001;
-      $d->save();
+        $d = new T923_Diagnostic();
+        $d->id_type = 101;
+        $d->id = 26;
+        $d->diagnostic_id = 75444;
+        $d->diag_timestamp = '2008-03-27 12:00:00';
+        $d->operator_id = 1001;
+        $d->save();
       
-      $d = new T923_Diagnostic();
-      $d->id_type = 101;
-      $d->id = 26;
-      $d->diagnostic_id = 75445;
-      $d->diag_timestamp = '2008-03-27 13:00:00';
-      $d->operator_id = 1001;
-      $d->save();
+        $d = new T923_Diagnostic();
+        $d->id_type = 101;
+        $d->id = 26;
+        $d->diagnostic_id = 75445;
+        $d->diag_timestamp = '2008-03-27 13:00:00';
+        $d->operator_id = 1001;
+        $d->save();
       
-      $d = new T923_Diagnostic();
-      $d->id_type = 101;
-      $d->id = 28;
-      $d->diagnostic_id = 75445;
-      $d->diag_timestamp = '2008-03-27 14:00:00';
-      $d->operator_id = 1001;
-      $d->save();
-      
+        $d = new T923_Diagnostic();
+        $d->id_type = 101;
+        $d->id = 28;
+        $d->diagnostic_id = 75445;
+        $d->diag_timestamp = '2008-03-27 14:00:00';
+        $d->operator_id = 1001;
+        $d->save();
     }
     
     public function prepareTables() {
@@ -67,21 +66,21 @@ class Doctrine_Ticket_923_TestCase extends Doctrine_UnitTestCase {
     
     public function testTicket()
     {
-      try {
-        $q = new Doctrine_Query();
-        $result = $q->select('d.*')
-        ->from('T923_Diagnostic d')
-        ->where('d.diag_timestamp >= ? AND d.diag_timestamp <= ?', array('2008-03-27 00:00:00', '2008-03-27 23:00:00'))
-        ->addWhere('d.id_type = ?', array('101'))
-        ->orderBy('d.diag_timestamp')
-        ->limit(20)
-        ->offset(0)
-        ->execute();
-      } catch (Exception $e) {
-          $this->fail($e->getMessage());
-      }
+        try {
+          $q = new Doctrine_Query();
+          $result = $q->select('d.*')
+          ->from('T923_Diagnostic d')
+          ->where('d.diag_timestamp >= ? AND d.diag_timestamp <= ?', array('2008-03-27 00:00:00', '2008-03-27 23:00:00'))
+          ->addWhere('d.id_type = ?', array('101'))
+          ->orderBy('d.diag_timestamp')
+          ->limit(20)
+          ->offset(0)
+          ->execute();
+        } catch (Exception $e) {
+            $this->fail($e->getMessage());
+        }
 
-      $this->assertTrue($result->count() == 3);
+        $this->assertEqual($result->count(), 3);
     }
 }
 
@@ -89,12 +88,12 @@ class T923_Diagnostic extends Doctrine_Record
 {
   public function setTableDefinition()
   {
-    $this->setTableName('diagnostics');
-    $this->hasColumn('id_type', 'integer', 4);
-    $this->hasColumn('id', 'integer', 4);
-    $this->hasColumn('diagnostic_id', 'integer', 4);
-    $this->hasColumn('operator_id', 'integer', 4);
-    $this->hasColumn('diag_timestamp', 'timestamp', null);
+      $this->setTableName('diagnostics');
+      $this->hasColumn('id_type', 'integer', 4);
+      $this->hasColumn('id', 'integer', 4);
+      $this->hasColumn('diagnostic_id', 'integer', 4);
+      $this->hasColumn('operator_id', 'integer', 4);
+      $this->hasColumn('diag_timestamp', 'timestamp', null);
   }
 
   public function setUp()
