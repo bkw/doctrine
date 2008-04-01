1eb3749a7979af934424281fcb12d50c83b4d5ce
guilhermeblanco <guilhermeblanco@625475ce-881a-0410-a577-b389adb331d8>
2008-04-01 14:20:13 +0000

    Intermediate rework in DELETE statement in new parser
    git-svn-id: http://svn.doctrine-project.org/branches/experimental@4163 625475ce-881a-0410-a577-b389adb331d8

---
+++ jepso-dql-parser-rewrite/lib/Doctrine/Query/Production/VariableDeclaration.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.phpdoctrine.org>.
+ */
+
+/**
+ * VariableDeclaration = identifier [["AS"] IdentificationVariable]
+ *
+ * @package     Doctrine
+ * @subpackage  Query
+ * @author      Guilherme Blanco <guilhermeblanco@hotmail.com>
+ * @author      Janne Vanhala <jpvanhal@cc.hut.fi>
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link        http://www.phpdoctrine.org
+ * @since       1.0
+ * @version     $Revision$
+ */
+class Doctrine_Query_Production_VariableDeclaration extends Doctrine_Query_Production
+{
+    protected $_componentName;
+
+    protected $_componentAlias;
+
+
+    public function execute(array $params = array())
+    {
+        // VariableDeclaration = identifier [["AS"] IdentificationVariable]
+        $path = '';
+
+        $parserResult = $this->_parser->getParserResult();
+        $connection = $this->_parser->getConnection();
+
+        if ($this->_parser->match(Doctrine_Query_Token::T_IDENTIFIER)) {
+            $this->_componentName = $this->_parser->token['value'];
+            $path = $this->_componentName;
+
+            if ($parserResult->hasQueryComponent($this->_componentName)) {
+                $queryComponent = $parserResult->getQueryComponent($this->_componentName);
+                $metadata = $queryComponent['metadata'];
+            } else {
+                // get the connection for the component
+                $manager = Doctrine_Manager::getInstance();
+                if ($manager->hasConnectionForComponent($this->_componentName)) {
+                    $this->_parser->setConnection($manager->getConnectionForComponent($this->_componentName));
+                }
+
+                $conn = $this->_parser->getConnection();
+
+                try {
+                    $metadata = $conn->getMetadata($this->_componentName);
+                    $mapper = $conn->getMapper($this->_componentName);
+                } catch (Doctrine_Exception $e) {
+                    $this->_parser->semanticalError($e->getMessage());
+                }
+
+                $queryComponent = array(
+                    'metadata'  => $metadata,
+                    'mapper'    => $mapper,
+                    'map'       => null
+                );
+            }
+
+            $parent = $path;
+        }
+
+        if ($this->_isNextToken(Doctrine_Query_Token::T_AS)) {
+            $this->_parser->match(Doctrine_Query_Token::T_AS);
+            $this->_componentAlias = $this->IdentificationVariable();
+        } elseif ($this->_isNextToken(Doctrine_Query_Token::T_IDENTIFIER)) {
+            $this->_componentAlias = $this->IdentificationVariable();
+        } else {
+            $this->_componentAlias = $path;
+        }
+
+        $parserResult->setQueryComponent($this->_componentAlias, $queryComponent);
+
+        return $this;
+    }
+
+
+    public function buildSql()
+    {
+        $parserResult = $this->_parser->getParserResult();
+        $queryComponent = $parserResult->getQueryComponent($this->_componentAlias);
+
+        return $queryComponent['metadata']->getTableName(). ' ' . $this->_sqlBuilder->generateTableAlias();
+    }
+}
