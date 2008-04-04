87851c9ae8f13c3c1839efad7c02cff1c126dcbc
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-04-04 19:27:42 +0000

    Added coverage for ticket #734
    git-svn-id: http://svn.doctrine-project.org/branches/0.10@4185 625475ce-881a-0410-a577-b389adb331d8

---
+++ tests/Query/CopyTestCase.php
@@ -0,0 +1,109 @@
+<?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.phpdoctrine.org>.
+ */
+
+/**
+ * Doctrine_Query_Copy_TestCase
+ *
+ * @package     Doctrine
+ * @author      Konsta Vesterinen <kvesteri@cc.hut.fi>
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @category    Object Relational Mapping
+ * @link        www.phpdoctrine.org
+ * @since       1.0
+ * @version     $Revision$
+ */
+class Doctrine_Query_Copy_TestCase extends Doctrine_UnitTestCase 
+{
+    public function testQueryCopy()
+    {
+        $q = new MyCopyQuery();
+
+        $q->from('User u');
+        
+        $q2 = $q->copy();
+        
+        $this->assertEqual($q->getSql(), $q2->getSql());
+
+	    $this->assertEqual($q->getSql(), 'SELECT e.id AS e__id, e.name AS e__name, e.loginname AS e__loginname, e.password AS e__password, e.type AS e__type, e.created AS e__created, e.updated AS e__updated, e.email_id AS e__email_id FROM entity e WHERE e.id = 4 AND (e.type = 0) ORDER BY DESC LIMIT 20 OFFSET 10');
+    }
+
+	public function testQueryRemoveOrderByPart()
+	{
+		$q = new MyCopyQuery();
+		$q->from('User u');
+
+		$q->removeQueryPart('orderby');
+
+		$this->assertEqual($q->getSql(), 'SELECT e.id AS e__id, e.name AS e__name, e.loginname AS e__loginname, e.password AS e__password, e.type AS e__type, e.created AS e__created, e.updated AS e__updated, e.email_id AS e__email_id FROM entity e WHERE e.id = 4 AND (e.type = 0) LIMIT 20 OFFSET 10');
+
+	}
+
+	public function testQueryRemoveLimitPart()
+	{
+		$q = new MyCopyQuery();
+		$q->from('User u');
+
+		$q->removeQueryPart('limit');
+
+		$this->assertEqual($q->getSql(), 'SELECT e.id AS e__id, e.name AS e__name, e.loginname AS e__loginname, e.password AS e__password, e.type AS e__type, e.created AS e__created, e.updated AS e__updated, e.email_id AS e__email_id FROM entity e WHERE e.id = 4 AND (e.type = 0) ORDER BY DESC OFFSET 10');
+	}
+
+	public function testQueryRemoveOffsetPart()
+	{
+		$q = new MyCopyQuery();
+		$q->from('User u');
+
+		$q->removeQueryPart('offset');
+
+		$this->assertEqual($q->getSql(), 'SELECT e.id AS e__id, e.name AS e__name, e.loginname AS e__loginname, e.password AS e__password, e.type AS e__type, e.created AS e__created, e.updated AS e__updated, e.email_id AS e__email_id FROM entity e WHERE e.id = 4 AND (e.type = 0) ORDER BY DESC LIMIT 20');
+	}
+
+	public function testQuerySetLimitToZero()
+	{
+		$q = new MyCopyQuery();
+		$q->from('User u');
+
+		$q->limit(0);
+
+		$this->assertEqual($q->getSql(), 'SELECT e.id AS e__id, e.name AS e__name, e.loginname AS e__loginname, e.password AS e__password, e.type AS e__type, e.created AS e__created, e.updated AS e__updated, e.email_id AS e__email_id FROM entity e WHERE e.id = 4 AND (e.type = 0) ORDER BY DESC OFFSET 10');
+	}
+
+	public function testQuerySetOffsetToZero()
+	{
+		$q = new MyCopyQuery();
+		$q->from('User u');
+
+		$q->offset(0);
+
+		$this->assertEqual($q->getSql(), 'SELECT e.id AS e__id, e.name AS e__name, e.loginname AS e__loginname, e.password AS e__password, e.type AS e__type, e.created AS e__created, e.updated AS e__updated, e.email_id AS e__email_id FROM entity e WHERE e.id = 4 AND (e.type = 0) ORDER BY DESC LIMIT 20');
+	}	
+}
+
+class MyCopyQuery extends Doctrine_Query
+{
+    public function preQuery()
+    {
+        $this->where('u.id = 4');
+        $this->orderBy(' DESC');
+        $this->offset(10);
+        $this->limit(20);
+    }
+}
