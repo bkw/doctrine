2481ab9424b7cefe858ef231ada4633c52fb7114
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-04-18 14:20:20 +0000

    fixes #970
    git-svn-id: http://svn.doctrine-project.org/branches/0.11@4231 625475ce-881a-0410-a577-b389adb331d8

--- tests/run.php
+++ tests/run.php
@@ -156,6 +156,7 @@ $core->addTestCase(new Doctrine_Configurable_TestCase());
 $core->addTestCase(new Doctrine_Manager_TestCase());
 $core->addTestCase(new Doctrine_Connection_TestCase());
 $core->addTestCase(new Doctrine_Table_TestCase());
+$core->addTestCase(new Doctrine_Table_RemoveColumn_TestCase());
 $core->addTestCase(new Doctrine_UnitOfWork_TestCase());
 $core->addTestCase(new Doctrine_Collection_TestCase());
 $core->addTestCase(new Doctrine_Collection_Snapshot_TestCase());
