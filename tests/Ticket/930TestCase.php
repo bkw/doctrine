2fe53764a8374b16a5b4f466442033a16a9dd304
romanb <romanb@625475ce-881a-0410-a577-b389adb331d8>
2008-05-24 16:13:48 +0000

    Fixed #930.
    git-svn-id: http://svn.doctrine-project.org/branches/0.11@4415 625475ce-881a-0410-a577-b389adb331d8

--- tests/Ticket/930TestCase.php
+++ tests/Ticket/930TestCase.php
@@ -35,95 +35,78 @@ class Doctrine_Ticket_930_TestCase extends Doctrine_UnitTestCase {
    * prepareData
    */
 
-    public function prepareData()
-    {
-	    $oPerson = new T930_Person;
-	    $oPerson->name = 'David';
-	    $oPerson->Country->code = 'fr';
-	    $oPerson->Country->Translation['fr']->name = 'France';
-	    $oPerson->Country->Translation['fr']->people = 'Francais';
-	    $oPerson->JobPositions[0]->name = 'Webdeveloper';
-	    $oPerson->JobPositions[0]->Category->code = '1234';
-	    $oPerson->JobPositions[0]->Category->Translation['fr']->name = 'Développeur Web';
-	    $oPerson->JobPositions[0]->Category->Translation['en']->name = 'Webdeveloper';
+  public function prepareData()
+  {
+    $oPerson = new T930_Person;
+    $oPerson->name = 'David';
 
-	    $oPerson->JobPositions[1]->name = 'Webmaster';
-      $oPerson->JobPositions[1]->Category->code = '1234';
-      $oPerson->JobPositions[1]->Category->Translation['fr']->name = 'Webmaster';
-      $oPerson->JobPositions[1]->Category->Translation['en']->name = 'Webmaster';
-      $oPerson->save();
+    $oCategory = new T930_JobCategory;
+    $oCategory->code = '1234';
+    $oCategory->Translation['fr']->name = 'Developpement';
+    $oCategory->Translation['en']->name = 'Development';
+     
+    $oPerson->JobPositions[0]->name = 'Webdeveloper';
+    $oPerson->JobPositions[0]->Category = $oCategory;
+     
+    $oPerson->JobPositions[1]->name = 'Webmaster';
+    $oPerson->JobPositions[1]->Category = $oCategory;
 
-      $oPerson = new T930_Person;
-      $oPerson->name = 'Jonathan';
-      $oPerson->Country->code = 'us';
-      $oPerson->Country->Translation['fr']->name   = 'Etats Unis';
-      $oPerson->Country->Translation['fr']->people = 'Américains';
-      $oPerson->Country->Translation['en']->name   = 'United states';
-      $oPerson->Country->Translation['fr']->people = 'Americans';
+    $oPerson->save();
 
-	    $oPerson->save();
+  }
 
+  /**
+   * prepareTables
+   */
+
+  public function prepareTables()
+  {
+    $this->tables = array();
+    $this->tables[] = 'T930_Person';
+    $this->tables[] = 'T930_JobPosition';
+    $this->tables[] = 'T930_JobCategory';
+     
+    parent :: prepareTables();
+  }
+
+
+  /**
+   * Test the existence expected indexes
+   */
+
+  public function testTicket()
+  {
+      $queryCountBefore = $this->conn->count();
+    try {
+      $q = new Doctrine_Query();
+      $r = $q
+      ->select('P.id, J.name, C.code, T.name')
+      ->from('T930_Person P')
+      ->leftJoin('P.JobPositions J')
+      ->leftJoin('J.Category C')
+      ->leftJoin('C.Translation T WITH T.lang = ?', 'fr')
+      ->fetchArray();
+    } catch (Exception $e) {
+      $this->fail($e->geMessage());
     }
-
-    /**
-     * prepareTables
-     */
-
-    public function prepareTables()
-    {
-    	$this->tables = array();
-    	$this->tables[] = 'T930_Person';
-    	$this->tables[] = 'T930_Country';
-    	$this->tables[] = 'T930_JobPosition';
-    	$this->tables[] = 'T930_JobCategory';
-
-    	parent :: prepareTables();
-    }
-
-
-    /**
-     * Test the existence expected indexes
-     */
-
-    public function testTicket()
-    {
-        try {
-        $q = new Doctrine_Query();
-        $r = $q
-          ->select('P.id, Ct.id, T1.name, T1.people, J.name, C.code, T2.name')
-          ->from('T930_Person P')
-          ->leftJoin('P.Country Ct')
-          ->leftJoin('Ct.Translation T1 WITH T1.lang = ?', 'fr')
-          ->leftJoin('P.JobPositions J')
-          ->leftJoin('J.Category C')
-          ->leftJoin('C.Translation T2 WITH T2.lang = ?', 'fr')
-          //->where('P.name = ?', 'Jonathan')
-          ->fetchArray();
-        } catch (Exception $e) {
-          $this->fail($e->getMessage());
-        }
-
-        $this->assertTrue(isset($r[0]['Country']['Translation']['fr']['name']));
-    }
+    $this->assertEqual($queryCountBefore + 1, $this->conn->count());
+    $this->assertTrue(isset($r[0]['JobPositions'][0]['Category']['Translation']['fr']['name']));
+    $this->assertTrue(isset($r[0]['JobPositions'][1]['Category']['Translation']['fr']['name']));
+    $this->assertEqual($queryCountBefore + 1, $this->conn->count());
+  }
 }
 
 class T930_Person extends Doctrine_Record
 {
   public function setTableDefinition()
   {
-      $this->setTableName('T930_person');
-      $this->hasColumn('country_id', 'integer');
-      $this->hasColumn('name', 'string', 200);
+    $this->setTableName('T930_person');
+    $this->hasColumn('name', 'string', 200);
   }
 
   public function setUp()
   {
     parent :: setUp();
-    $this->hasOne('T930_Country as Country', array(
-      'local' => 'country_id',
-      'foreign' => 'id',
-      'onDelete' => 'CASCADE'
-      ));
 
     $this->hasMany('T930_JobPosition as JobPositions', array(
       'local' => 'id',
@@ -133,39 +116,15 @@ class T930_Person extends Doctrine_Record
   }
 }
 
-class T930_Country extends Doctrine_Record
-{
-  public function setTableDefinition()
-  {
-      $this->setTableName('T930_country');
-      $this->hasColumn('name', 'string', 200);
-      $this->hasColumn('people', 'string', 200);
-      $this->hasColumn('code', 'string', 200);
-  }
-
-  public function setUp()
-  {
-    parent :: setUp();
-    $this->hasMany('T930_Person as Persons', array(
-      'local' => 'id',
-      'foreign' => 'country_id',
-      'onDelete' => 'CASCADE'
-      ));
-
-    $this->actAs('I18n', array('fields' => array('name', 'people')));
-  }
-}
-
-
 
 class T930_JobPosition extends Doctrine_Record
 {
   public function setTableDefinition()
   {
-      $this->setTableName('T930_address');
-      $this->hasColumn('name', 'string', 200);
-      $this->hasColumn('person_id', 'integer');
-      $this->hasColumn('job_category_id', 'integer');
+    $this->setTableName('T930_address');
+    $this->hasColumn('name', 'string', 200);
+    $this->hasColumn('person_id', 'integer');
+    $this->hasColumn('job_category_id', 'integer');
   }
 
   public function setUp()
@@ -177,7 +136,7 @@ class T930_JobPosition extends Doctrine_Record
       'onDelete' => 'CASCADE'
       ));
 
-    $this->hasOne('T930_JobCategory as Category', array(
+      $this->hasOne('T930_JobCategory as Category', array(
       'local' => 'job_category_id',
       'foreign' => 'id',
       'onDelete' => 'CASCADE'
@@ -189,9 +148,9 @@ class T930_JobCategory extends Doctrine_Record
 {
   public function setTableDefinition()
   {
-      $this->setTableName('job_category');
-      $this->hasColumn('code', 'integer', 4);
-      $this->hasColumn('name', 'string', 200);
+    $this->setTableName('job_category');
+    $this->hasColumn('code', 'integer', 4);
+    $this->hasColumn('name', 'string', 200);
   }
 
   public function setUp()
@@ -203,6 +162,10 @@ class T930_JobCategory extends Doctrine_Record
       'onDelete' => 'CASCADE'
       ));
 
-    $this->actAs('I18n', array('fields' => array('name')));
+      $this->actAs('I18n', array('fields' => array('name')));
   }
 }
+
+
+
+
