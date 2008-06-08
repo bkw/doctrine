16a5932a1e46981a2ad4dae91dc8bdefa49d20da
guilhermeblanco <guilhermeblanco@625475ce-881a-0410-a577-b389adb331d8>
2008-06-08 17:59:34 +0000

    Fixes 1116. Actually it is a PHP bug that was only solved in 6 march 2008. So only possible release to have this fixed is using PHP 5.2.6. Please refer to http://bugs.php.net/bug.php?id=40417 for more explanation. I updated the test case to be sure we are correctly passing things to MySQL. Updated Query and QueryAbstract to use getParams as they should. Also, getParams only displays the correct number of params after preQuery event. SoftDelete binds new values only after processing DQL (which is done calling getSql()), so getParams only has the right values after it.
    git-svn-id: http://svn.doctrine-project.org/branches/0.11@4488 625475ce-881a-0410-a577-b389adb331d8

--- tests/Ticket/1116TestCase.php
+++ tests/Ticket/1116TestCase.php
@@ -6,39 +6,47 @@ class Doctrine_Ticket_1116_TestCase extends Doctrine_UnitTestCase
 		//switch to a real db to trigger the Exception
 		$this->dbh = new Doctrine_Adapter_Mock('mysql');
 		//$this->dbh = new PDO("mysql:host=localhost;dbname=testing", 'root', 'password');
-		  
+
 		$this->conn = Doctrine_Manager::getInstance()->openConnection($this->dbh);
 		$this->conn->export->exportClasses(array('Ticket_1116_User'));
 	}
+
+
 	public function testTicket()
 	{
 		$q = new Doctrine_Query();
 		$q->select('s.*')
 		  ->from('Ticket_1116_User s')
 		  ->where('s.username = ?', array('test'));
-		  
+
 		// to see the error switch dbh to a real db, the next line will trigger the error
 		$test = $q->fetchOne();  //will only fail with "real" mysql 
-		$this->assertFalse($test);		  
-		  	  
+		$this->assertFalse($test);
+
 		$sql    = $q->getSql(); // just getSql()?!?! and it works ? the params are ok after this call  
-		$params = $q->getParams();       
-		$this->assertEqual(count($params),2); // now we have array('test',null) very strange ..... 
-		 
+		$params = $q->getParams();
+		$this->assertEqual(count($params), 2); // now we have array('test',null) very strange ..... 
+
+		$this->assertEqual($sql, "SELECT u.id AS u__id, u.username AS u__username, u.deleted AS u__deleted FROM user u WHERE u.username = ? AND u.deleted = ?");
+		$this->assertEqual($params, array('test', false));
+
 		//now also this works! (always works witch mock only fails with mysql)
 		$test = $q->fetchOne();
-		$this->assertFalse($test);	     
+		$this->assertFalse($test);
 	}
 }
 
+
 class Ticket_1116_User extends Doctrine_Record
 {
-  public function setTableDefinition()
+	public function setTableDefinition()
 	{
 		$this->setTableName('user');
 		$this->hasColumn('id', 'integer', 4, array('primary' => true, 'autoincrement' => true));
 		$this->hasColumn('username', 'string', 255);
 	}
+
+
 	public function setUp()
 	{
 		parent::setUp();
