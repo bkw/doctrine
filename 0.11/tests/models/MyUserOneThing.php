80f06be06a913a1565838b4e155d17803cf5eb82
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-06-11 16:18:31 +0000

    Removed error from creating 0.11.0-RC4 tag.
    git-svn-id: http://svn.doctrine-project.org/tags/0.11.0-RC4@4511 625475ce-881a-0410-a577-b389adb331d8

--- 0.11/tests/models/MyUserOneThing.php
+++
@@ -1,7 +0,0 @@
-<?php
-class MyUserOneThing extends Doctrine_Record {
-    public function setTableDefinition() {
-        $this->hasColumn('user_id', 'integer');
-        $this->hasColumn('one_thing_id', 'integer');
-    }
-}
--- 0.11/tests/models/MyUserOtherThing.php
+++
@@ -1,7 +0,0 @@
-<?php
-class MyUserOtherThing extends Doctrine_Record {
-    public function setTableDefinition() {
-        $this->hasColumn('user_id', 'integer');
-        $this->hasColumn('other_thing_id', 'integer');
-    }
-}
