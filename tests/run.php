c10e11bbeced413781970fee3aa81f77f2fc351f
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-06-13 18:01:31 +0000

    fixes #1133
    git-svn-id: http://svn.doctrine-project.org/branches/0.11@4519 625475ce-881a-0410-a577-b389adb331d8

--- tests/run.php
+++ tests/run.php
@@ -79,6 +79,7 @@ $tickets->addTestCase(new Doctrine_Ticket_1116_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1121_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1134_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1118_TestCase());
+$tickets->addTestCase(new Doctrine_Ticket_1133_TestCase());
 
 $test->addTestCase($tickets);
 
