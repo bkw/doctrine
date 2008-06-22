84feb31b77043cfc1c8d9e6378c358ab668f3878
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-06-22 03:46:30 +0000

    Fix.
    git-svn-id: http://svn.doctrine-project.org/branches/0.11@4545 625475ce-881a-0410-a577-b389adb331d8

--- vendor/Sensei/Sensei/Doc/Section.php
+++ vendor/Sensei/Sensei/Doc/Section.php
@@ -393,9 +393,9 @@ class Sensei_Doc_Section implements Countable
      */
     public static function convertNameToPath($name)
     {
-        $patterns = '/\s/';
-        $replacements = '-';
-            
+        $patterns = array('/\s/', '/\(|\)/');
+        $replacements = array('-', '');
+
         return preg_replace($patterns, $replacements, strtolower($name)); 
     }
 }
