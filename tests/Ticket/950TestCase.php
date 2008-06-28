18c01510a1e5f6daf5af680ba43f447ca70d180b
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-06-28 08:08:41 +0000

    Making test fail.
    git-svn-id: http://svn.doctrine-project.org/branches/0.11@4585 625475ce-881a-0410-a577-b389adb331d8

--- tests/Ticket/950TestCase.php
+++ tests/Ticket/950TestCase.php
@@ -41,11 +41,10 @@ class Doctrine_Ticket_950_TestCase extends Doctrine_UnitTestCase
     public function testTest()
     {
         $sql = $this->conn->export->exportClassesSql(array('Ticket_950_AdresseRecord','Ticket_950_CountryRecord'));
-        $this->assertEqual(count($sql), 4);
+        $this->assertEqual(count($sql), 3);
         $this->assertEqual($sql[0], 'CREATE TABLE country_record (id BIGINT NOT NULL AUTO_INCREMENT, iso VARCHAR(2) NOT NULL, name VARCHAR(80), printable_name VARCHAR(80), iso3 VARCHAR(3), numcode BIGINT, INDEX iso_idx (iso), PRIMARY KEY(id)) ENGINE = INNODB');
         $this->assertEqual($sql[1], 'CREATE TABLE adresse_record (id BIGINT NOT NULL AUTO_INCREMENT, adresse VARCHAR(255), cp VARCHAR(60), ville VARCHAR(255), pays VARCHAR(2), INDEX pays_idx (pays), PRIMARY KEY(id)) ENGINE = INNODB');
-        $this->assertEqual($sql[2], 'ALTER TABLE country_record ADD FOREIGN KEY (iso) REFERENCES adresse_record(pays)');
-        $this->assertEqual($sql[3], 'ALTER TABLE adresse_record ADD FOREIGN KEY (pays) REFERENCES country_record(iso)');
+        $this->assertEqual($sql[2], 'ALTER TABLE adresse_record ADD FOREIGN KEY (pays) REFERENCES country_record(iso)');
     }
 }
 
@@ -85,10 +84,5 @@ class Ticket_950_CountryRecord extends Doctrine_Record
 		$this->hasColumn('printable_name', 'string', 80);
 		$this->hasColumn('iso3', 'string', 3);
 		$this->hasColumn('numcode', 'integer', 10);
-	}
-
-	public function setUp()
-	{
-		$this->hasOne('Ticket_950_AdresseRecord as Address', array('local' => 'iso', 'foreign' => 'pays'));
 	}
 }
