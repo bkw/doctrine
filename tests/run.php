f69670ba16f78f2c6eb19416238178c60b4a6ac4
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-07-08 14:50:38 +0000

    Adding coverage for #1192
    git-svn-id: http://svn.doctrine-project.org/branches/0.11@4633 625475ce-881a-0410-a577-b389adb331d8

--- tests/run.php
+++ tests/run.php
@@ -84,6 +84,7 @@ $tickets->addTestCase(new Doctrine_Ticket_1123_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1124_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1133_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1175_TestCase());
+$tickets->addTestCase(new Doctrine_Ticket_1192_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1205_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1206_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1215_TestCase());
