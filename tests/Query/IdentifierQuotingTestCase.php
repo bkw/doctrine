584ed6cdccffab3829afa15193b6c775aeb13d72
guilhermeblanco <guilhermeblanco@625475ce-881a-0410-a577-b389adb331d8>
2008-07-09 03:44:41 +0000

    Fixed #1209 and added coverage to it. Added new methods: Doctrine_Connection->quoteMultipleIdentifier and Doctrine_Formatter->quoteMultipleIdentifier.
    git-svn-id: http://svn.doctrine-project.org/branches/0.11@4645 625475ce-881a-0410-a577-b389adb331d8

--- tests/Query/IdentifierQuotingTestCase.php
+++ tests/Query/IdentifierQuotingTestCase.php
@@ -97,6 +97,15 @@ class Doctrine_Query_IdentifierQuoting_TestCase extends Doctrine_UnitTestCase
         $q->parseQuery('SELECT u.name FROM User u INNER JOIN u.Phonenumber p')->limit(5);
 
         $this->assertEqual($q->getQuery(), 'SELECT "e"."id" AS "e__id", "e"."name" AS "e__name" FROM "entity" "e" INNER JOIN "phonenumber" "p" ON "e"."id" = "p"."entity_id" WHERE "e"."id" IN (SELECT DISTINCT "e2"."id" FROM "entity" "e2" INNER JOIN "phonenumber" "p2" ON "e2"."id" = "p2"."entity_id" WHERE ("e2"."type" = 0) LIMIT 5) AND ("e"."type" = 0)');
+    }
+    
+    public function testCountQuerySupportsIdentifierQuoting()
+    {
+        $q = new Doctrine_Query();
+
+        $q->parseQuery('SELECT u.name FROM User u INNER JOIN u.Phonenumber p');
+        
+        $this->assertEqual($q->getCountQuery(), 'SELECT COUNT(DISTINCT "e"."id") AS num_results FROM "entity" "e" INNER JOIN "phonenumber" "p" ON "e"."id" = "p"."entity_id" WHERE "e"."type" = 0 GROUP BY "e"."id"');
 
         $this->conn->setAttribute(Doctrine::ATTR_QUOTE_IDENTIFIER, false);
     }
