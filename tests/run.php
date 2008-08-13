97b25218bd1402a44876f423f14134eef4354c4c
gnat <gnat@625475ce-881a-0410-a577-b389adb331d8>
2008-08-13 21:10:25 +0000

    adding testcase Ticket: 1339
    git-svn-id: http://svn.doctrine-project.org/branches/0.11@4771 625475ce-881a-0410-a577-b389adb331d8

--- tests/run.php
+++ tests/run.php
@@ -96,6 +96,7 @@ $tickets->addTestCase(new Doctrine_Ticket_1225_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1228_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1251_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1254_TestCase());
+//$tickets->addTestCase(new Doctrine_Ticket_1339_TestCase());
 
 $test->addTestCase($tickets);
 
