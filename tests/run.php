d8de216b4652f487320bc929b336d2059b4e258b
guilhermeblanco <guilhermeblanco@625475ce-881a-0410-a577-b389adb331d8>
2008-08-27 05:44:41 +0000

    Completed Doctrine_Query_Registry support. Included named query support, backported from trunk. From now on you're able to define and run named queries from *Table classes. Documentation to be added.
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@4845 625475ce-881a-0410-a577-b389adb331d8

--- tests/run.php
+++ tests/run.php
@@ -223,6 +223,7 @@ $core->addTestCase(new Doctrine_Manager_TestCase());
 $core->addTestCase(new Doctrine_Connection_TestCase());
 $core->addTestCase(new Doctrine_Table_TestCase());
 $core->addTestCase(new Doctrine_Table_RemoveColumn_TestCase());
+$core->addTestCase(new Doctrine_Table_NamedQuery_TestCase());
 $core->addTestCase(new Doctrine_UnitOfWork_TestCase());
 $core->addTestCase(new Doctrine_Collection_TestCase());
 $core->addTestCase(new Doctrine_Collection_Snapshot_TestCase());
