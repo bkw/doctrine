a6253e6a47940cfc87023887df07725f75ab0b3b
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-09-08 21:02:31 +0000

    Updating test to include Searchable as I18n child
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@4893 625475ce-881a-0410-a577-b389adb331d8

--- tests/Ticket/1351TestCase.php
+++ tests/Ticket/1351TestCase.php
@@ -71,6 +71,9 @@ class Ticket_1351_Article extends Doctrine_Record
 
     public function setUp()
     {
-        $this->actAs('I18n', array('fields' => array('title', 'body')));
+        $i18n0 = new Doctrine_Template_I18n(array('fields' => array(0 => 'title', 1 => 'body')));
+        $searchable1 = new Doctrine_Template_Searchable(array('fields' => array(0 => 'title')));
+        $i18n0->addChild($searchable1);
+        $this->actAs($i18n0);
     }
 }
