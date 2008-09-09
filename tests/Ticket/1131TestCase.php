1b9839a44b185d99b8efb85ce08045a205462c72
jasoneisen <jasoneisen@625475ce-881a-0410-a577-b389adb331d8>
2008-09-09 15:34:04 +0000

    Adding coverage for #1131
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@4900 625475ce-881a-0410-a577-b389adb331d8

--- tests/Ticket/1131TestCase.php
+++ tests/Ticket/1131TestCase.php
@@ -37,6 +37,7 @@ class Doctrine_Ticket_1131_TestCase extends Doctrine_UnitTestCase
         //$this->tables = array();
         $this->tables[] = 'Ticket_1131_User';
         $this->tables[] = 'Ticket_1131_Group';
+        $this->tables[] = 'Ticket_1131_Role';
         parent::prepareTables();
     }
     
@@ -45,20 +46,33 @@ class Doctrine_Ticket_1131_TestCase extends Doctrine_UnitTestCase
     {
         parent::prepareData();
         
+        $role = new Ticket_1131_Role();
+        $role->name = 'Role One';
+        $role->save();
+        $this->role_one = $role->id;
+        
+        $role = new Ticket_1131_Role();
+        $role->name = 'Role Two';
+        $role->save();
+        $this->role_two = $role->id;
+        
         $group = new Ticket_1131_Group();
+        $group->role_id = $this->role_one;
         $group->name = 'Core Dev';
         $group->save();
 
         $user = new Ticket_1131_User();
         $user->Group = $group;
+        $user->role_id = $this->role_two;
         $user->name = 'jwage';
         $user->save();
         
+        $role->free();
         $group->free();
         $user->free();
     }
 
-    public function testTicket()
+    public function testOriginalTicket()
     {
         $user = Doctrine_Query::create()
             ->from('Ticket_1131_User u')
@@ -67,6 +81,71 @@ class Doctrine_Ticket_1131_TestCase extends Doctrine_UnitTestCase
         $this->assertEqual($user->Group->id, 1);
         $this->assertFalse($user->get('group_id') instanceof Doctrine_Record);
     }
+    
+    public function testOriginalTicketWithJoins()
+    {
+        $user = Doctrine_Query::create()
+            ->from('Ticket_1131_User u')
+            ->leftJoin('u.Group g')
+            ->where('u.id = ?')->fetchOne(array(1));
+
+        $this->assertEqual($user->Group->id, 1);
+        $this->assertFalse($user->get('group_id') instanceof Doctrine_Record);
+    }
+    
+    public function testOverloading()
+    {
+        $orig = Doctrine_Manager::getInstance()->getAttribute('auto_accessor_override');
+        Doctrine_Manager::getInstance()->setAttribute('auto_accessor_override', true);
+        
+        $user = Doctrine_Query::create()
+            ->from('Ticket_1131_User u')
+            ->where('u.id = ?')->fetchOne(array(1));
+        
+        $this->assertEqual($user->group_id, 1);
+        $this->assertEqual($user->get('group_id'), 1);
+        $this->assertFalse($user->get('group_id') instanceof Doctrine_Record);
+        
+        $this->assertEqual($user->role_id, 2);
+        $this->assertEqual($user->get('role_id'), 2);
+        $this->assertFalse($user->get('role_id') instanceof Doctrine_Record);
+        
+        $this->assertEqual($user->Group->id, 1);
+        $this->assertEqual($user->get('Group')->get('id'), 1);
+        
+        $this->assertEqual($user->Role->id, 2);
+        $this->assertEqual($user->get('Role')->get('id'), 2);
+        
+        Doctrine_Manager::getInstance()->setAttribute('auto_accessor_override', $orig);
+    }
+    
+    public function testOverloadingWithJoins()
+    {
+        $orig = Doctrine_Manager::getInstance()->getAttribute('auto_accessor_override');
+        Doctrine_Manager::getInstance()->setAttribute('auto_accessor_override', true);
+        
+        $user = Doctrine_Query::create()
+            ->from('Ticket_1131_UserWithOverloading u')
+            ->leftJoin('u.Group g')
+            ->leftJoin('u.Role r')
+            ->addWhere('u.id = ?')->fetchOne(array(1));
+        
+        $this->assertEqual($user->group_id, 1);
+        $this->assertEqual($user->get('group_id'), 1);
+        $this->assertFalse($user->get('group_id') instanceof Doctrine_Record);
+        
+        $this->assertEqual($user->role_id, 1);
+        $this->assertEqual($user->get('role_id'), 1);
+        $this->assertFalse($user->get('role_id') instanceof Doctrine_Record);
+        
+        $this->assertEqual($user->Group->id, 1);
+        $this->assertEqual($user->get('Group')->get('id'), 1);
+        
+        $this->assertEqual($user->Role->id, 1);
+        $this->assertEqual($user->get('Role')->get('id'), 1);
+        
+        Doctrine_Manager::getInstance()->setAttribute('auto_accessor_override', $orig);
+    }
 }
 
 class Ticket_1131_User extends Doctrine_Record
@@ -74,6 +153,9 @@ class Ticket_1131_User extends Doctrine_Record
     public function setTableDefinition()
     {
         $this->hasColumn('group_id', 'integer', 20, array(
+            'notnull' => false, 'default' => null
+        ));
+        $this->hasColumn('role_id', 'integer', 20, array(
             'notnull' => false, 'default' => null
         ));
         $this->hasColumn('name', 'string', 255);
@@ -85,11 +167,50 @@ class Ticket_1131_User extends Doctrine_Record
             'local' => 'group_id',
             'foreign' => 'id'
         ));
+        
+        $this->hasOne('Ticket_1131_Role as Role', array(
+            'local' => 'role_id',
+            'foreign' => 'id'));
     }
 }
 
+class Ticket_1131_UserWithOverloading extends Ticket_1131_User
+{
+    public function getRole()
+    {
+        return $this->Group->Role;
+    }
+    
+    public function getRoleId()
+    {
+        return $this->Group->role_id;
+    }
+}
 
 class Ticket_1131_Group extends Doctrine_Record
+{
+    public function setTableDefinition()
+    {
+        $this->hasColumn('role_id', 'integer', 20, array(
+            'notnull' => false, 'default' => null
+        ));
+        $this->hasColumn('name', 'string', 255);
+    }
+
+    public function setUp()
+    {
+        $this->hasOne('Ticket_1131_Role as Role', array(
+            'local' => 'role_id',
+            'foreign' => 'id'));
+        
+        $this->hasMany('Ticket_1131_User as Users', array(
+            'local' => 'id',
+            'foreign' => 'group_id'
+        ));
+    }
+}
+
+class Ticket_1131_Role extends Doctrine_Record
 {
     public function setTableDefinition()
     {
@@ -100,7 +221,11 @@ class Ticket_1131_Group extends Doctrine_Record
     {
         $this->hasMany('Ticket_1131_User as Users', array(
             'local' => 'id',
-            'foreign' => 'group_id'
+            'foreign' => 'role_id'
+        ));
+        $this->hasMany('Ticket_1131_Group as Groups', array(
+            'local' => 'id',
+            'foreign' => 'role_id'
         ));
     }
 }
