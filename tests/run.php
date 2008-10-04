57995acb5fb285ad1aac72320669367fc7023f33
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-10-04 21:56:13 +0000

    [1.0, 1.1] Added coverage for #1523
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@5052 625475ce-881a-0410-a577-b389adb331d8

--- tests/run.php
+++ tests/run.php
@@ -145,6 +145,7 @@ $tickets->addTestCase(new Doctrine_Ticket_1465_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1480_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1483_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1507_TestCase());
+$tickets->addTestCase(new Doctrine_Ticket_1523_TestCase());
 $test->addTestCase($tickets);
 
 // Connection Tests (not yet fully tested)
