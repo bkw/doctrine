576a22a0b9214b1ba01af4989c1823eb0ab66fc4
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-10-07 21:17:59 +0000

    [1.0, 1.1] fixes #1545 - fixes issue with record filters and null values
    git-svn-id: http://svn.doctrine-project.org/branches/1.1@5065 625475ce-881a-0410-a577-b389adb331d8

--- tests/run.php
+++ tests/run.php
@@ -154,6 +154,7 @@ $tickets->addTestCase(new Doctrine_Ticket_1480_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1483_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1494_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1507_TestCase());
+$tickets->addTestCase(new Doctrine_Ticket_1520_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1522_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1523_TestCase());
 $tickets->addTestCase(new Doctrine_Ticket_1527_TestCase());
