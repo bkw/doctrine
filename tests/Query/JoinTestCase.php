e9cd4b6367d959706205a3c70506364f00796f64
guilhermeblanco <guilhermeblanco@625475ce-881a-0410-a577-b389adb331d8>
2008-10-08 06:44:26 +0000

    [1.0, 1.1] fixes #1483, #1488, #1513, #1514. Improved JoinCondition handling. Added possibility to use placeholders in functions.
    git-svn-id: http://svn.doctrine-project.org/branches/1.1@5066 625475ce-881a-0410-a577-b389adb331d8

--- tests/Query/JoinTestCase.php
+++ tests/Query/JoinTestCase.php
@@ -113,6 +113,32 @@ class Doctrine_Query_Join_TestCase extends Doctrine_UnitTestCase
 
         $this->assertEqual($q->getSql(), 'SELECT r.id AS r__id, r.name AS r__name, r2.id AS r2__id, r2.name AS r2__name, r2.country_id AS r2__country_id, r2.district_id AS r2__district_id FROM record__country r INNER JOIN record__city r2 ON r.id = r2.country_id AND LOWER(UPPER(r2.name)) LIKE LOWER(?) WHERE r.id = ?');
     }
+    
+    
+    public function testQueryMultipleAggFunctionInJoins2()
+    {
+        $q = new Doctrine_Query();
+
+        $q->select('c.*, c2.*, d.*')
+          ->from('Record_Country c')
+          ->innerJoin('c.City c2 WITH LOWER(UPPER(c2.name)) LIKE CONCAT(UPPER(?), UPPER(c2.name))', array('city 1'))
+          ->where('c.id = ?', array(1));
+
+        $this->assertEqual($q->getSql(), 'SELECT r.id AS r__id, r.name AS r__name, r2.id AS r2__id, r2.name AS r2__name, r2.country_id AS r2__country_id, r2.district_id AS r2__district_id FROM record__country r INNER JOIN record__city r2 ON r.id = r2.country_id AND LOWER(UPPER(r2.name)) LIKE CONCAT(UPPER(?), UPPER(r2.name)) WHERE r.id = ?');
+    }
+    
+    
+    public function testQueryMultipleAggFunctionInJoins3()
+    {
+        $q = new Doctrine_Query();
+
+        $q->select('c.*, c2.*, d.*')
+          ->from('Record_Country c')
+          ->innerJoin('c.City c2 WITH CONCAT(UPPER(c2.name), c2.name) LIKE UPPER(?)', array('CITY 1city 1'))
+          ->where('c.id = ?', array(1));
+
+        $this->assertEqual($q->getSql(), 'SELECT r.id AS r__id, r.name AS r__name, r2.id AS r2__id, r2.name AS r2__name, r2.country_id AS r2__country_id, r2.district_id AS r2__district_id FROM record__country r INNER JOIN record__city r2 ON r.id = r2.country_id AND CONCAT(UPPER(r2.name), r2.name) LIKE UPPER(?) WHERE r.id = ?');
+    }
 
 
     public function testQueryWithInInsideJoins()
--- tests/Ticket/1483TestCase.php
+++ tests/Ticket/1483TestCase.php
@@ -40,6 +40,24 @@ class Doctrine_Ticket_1483_TestCase extends Doctrine_UnitTestCase
         $this->assertEqual($q->getSql(), 'SELECT t.id AS t__id, t.username AS t__username, t2.id AS t2__id, t2.name AS t2__name FROM ticket_1483__user t LEFT JOIN ticket_1483__user_group t3 ON t.id = t3.user_id LEFT JOIN ticket_1483__group t2 ON t2.id = t3.group_id AND t2.id = (SELECT t4.id AS t4__id FROM ticket_1483__group t4 WHERE (t4.name = \'Test\' OR t4.name = \'Test2\'))');
 
     }
+    
+    public function testTest2()
+    {
+        $q = Doctrine_Query::create()
+            ->from('Ticket_1483_User u')
+            ->leftJoin('u.Groups g WITH g.id = (SELECT g2.id FROM Ticket_1483_Group g2 WHERE (g2.name = \'Test\' OR (g2.name = \'Test2\')))');
+        $this->assertEqual($q->getSql(), 'SELECT t.id AS t__id, t.username AS t__username, t2.id AS t2__id, t2.name AS t2__name FROM ticket_1483__user t LEFT JOIN ticket_1483__user_group t3 ON t.id = t3.user_id LEFT JOIN ticket_1483__group t2 ON t2.id = t3.group_id AND t2.id = (SELECT t4.id AS t4__id FROM ticket_1483__group t4 WHERE (t4.name = \'Test\' OR t4.name = \'Test2\'))');
+
+    }
+    
+    public function testTest3()
+    {
+        $q = Doctrine_Query::create()
+            ->from('Ticket_1483_User u')
+            ->leftJoin('u.Groups g WITH g.id = (SELECT g2.id FROM Ticket_1483_Group g2 WHERE ((g2.name = \'Test\' AND g2.name = \'Test2\') OR (g2.name = \'Test2\')))');
+        $this->assertEqual($q->getSql(), 'SELECT t.id AS t__id, t.username AS t__username, t2.id AS t2__id, t2.name AS t2__name FROM ticket_1483__user t LEFT JOIN ticket_1483__user_group t3 ON t.id = t3.user_id LEFT JOIN ticket_1483__group t2 ON t2.id = t3.group_id AND t2.id = (SELECT t4.id AS t4__id FROM ticket_1483__group t4 WHERE ((t4.name = \'Test\' AND t4.name = \'Test2\') OR t4.name = \'Test2\'))');
+
+    }
 }
 
 class Ticket_1483_User extends Doctrine_Record
