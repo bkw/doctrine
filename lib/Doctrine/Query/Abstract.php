5e94340ba395cfba13601073b275f230fcb90c73
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2008-11-20 21:05:23 +0000

    [1.0, 1.1] Fixes issue with result cache not respecting passed ttl (closes #1686)
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@5196 625475ce-881a-0410-a577-b389adb331d8

--- lib/Doctrine/Query/Abstract.php
+++ lib/Doctrine/Query/Abstract.php
@@ -1844,7 +1844,10 @@ abstract class Doctrine_Query_Abstract
         }
         $this->_resultCache = $driver;
 
-        return $this->setResultCacheLifeSpan($timeToLive);
+        if ($timeToLive !== null) {
+            $this->setResultCacheLifeSpan($timeToLive);
+        }
+        return $this;
     }
 
     /**
@@ -1854,7 +1857,7 @@ abstract class Doctrine_Query_Abstract
      * @param integer $timeToLive                        how long the cache entry is valid
      * @return Doctrine_Hydrate         this object
      */
-    public function useQueryCache($driver = true, $timeToLive = false)
+    public function useQueryCache($driver = true, $timeToLive = null)
     {
         if ($driver !== null && $driver !== true && $driver !== false && ! ($driver instanceOf Doctrine_Cache_Interface)) {
             $msg = 'First argument should be instance of Doctrine_Cache_Interface or null.';
@@ -1862,10 +1865,9 @@ abstract class Doctrine_Query_Abstract
         }
         $this->_queryCache = $driver;
 
-        if ($timeToLive !== false) { 
-            $this->setResultCacheLifeSpan($timeToLive); 
-        } 
-
+        if ($timeToLive !== null) {
+            $this->setResultCacheLifeSpan($timeToLive);
+        }
         return $this;
     }
 
