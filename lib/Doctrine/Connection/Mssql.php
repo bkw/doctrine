d63cdc3674b8247d3ab98e592ce9a4f5ef387e49
guilhermeblanco <guilhermeblanco@625475ce-881a-0410-a577-b389adb331d8>
2009-01-31 22:44:38 +0000

    [1.0, 1.1] Fixed #1835. Fixed MSSQL limit-subquery that was incorrectly using alias and also double escaping it. Thanks for the patch
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@5448 625475ce-881a-0410-a577-b389adb331d8

--- lib/Doctrine/Connection/Mssql.php
+++ lib/Doctrine/Connection/Mssql.php
@@ -121,33 +121,29 @@ class Doctrine_Connection_Mssql extends Doctrine_Connection
             $orderby = stristr($query, 'ORDER BY');
 
             if ($orderby !== false) {
-                $sort = (stripos($orderby, 'desc') !== false) ? 'desc' : 'asc';
+                $sort = (stripos($orderby, ' desc') !== false) ? 'desc' : 'asc';
                 $order = str_ireplace('ORDER BY', '', $orderby);
-                $order = trim(preg_replace('/ASC|DESC/i', '', $order));
-                $alias = trim(end(spliti(' as ', array_shift(explode(',', stristr($query,$order))))));
+                $order = trim(preg_replace('/\s+(ASC|DESC)$/i', '', $order));
+                
+                // Ticket #1835: Fix for ORDER BY alias
+                $aux = explode(',', stristr($query,$order));
+                $aux2 = spliti(' as ', array_shift($aux));
+                
+                $alias = trim(end($aux2));
             }
-
-            // Ticket #1259: Fix for limit-subquery in MSSQL
-            $selectRegExp = 'SELECT\s+';
-            $selectReplace = 'SELECT ';
-
-            if (preg_match('/^SELECT(\s+)DISTINCT/i',  $query)) {
-                $selectRegExp .= 'DISTINCT\s+';
-                $selectReplace .= 'DISTINCT ';
-            }
-
-            $query = preg_replace('/^'.$selectRegExp.'\s+/i', $selectReplace . 'TOP ' . ($count+$offset) . ' ', $query);    
+    
+            $query = preg_replace('/^SELECT\s/i', 'SELECT TOP ' . ($count+$offset) . ' ', $query);    
             $query = 'SELECT * FROM (SELECT TOP ' . $count . ' * FROM (' . $query . ') AS ' . $this->quoteIdentifier('inner_tbl');
 
             if ($orderby !== false) {
-                $query .= ' ORDER BY ' . $this->quoteIdentifier('inner_tbl.' . $alias) . ' ';
+                $query .= ' ORDER BY ' . $this->quoteIdentifier('inner_tbl') . '.' . $alias . ' ';
                 $query .= (stripos($sort, 'asc') !== false) ? 'DESC' : 'ASC';
             }
 
             $query .= ') AS ' . $this->quoteIdentifier('outer_tbl');
 
             if ($orderby !== false) {
-                $query .= ' ORDER BY ' . $this->quoteIdentifier('outer_tbl.' . $alias) . ' ' . $sort;
+                $query .= ' ORDER BY ' . $this->quoteIdentifier('outer_tbl') . '.' . $alias . ' ' . $sort;
             }
         }
 
