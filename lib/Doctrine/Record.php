68a204a6988308b0a9a792009e499a50721a6364
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2009-02-05 00:03:51 +0000

    [1.0, 1.1] Fixes mistake from previous commit.
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@5472 625475ce-881a-0410-a577-b389adb331d8

--- lib/Doctrine/Record.php
+++ lib/Doctrine/Record.php
@@ -2054,8 +2054,8 @@ abstract class Doctrine_Record extends Doctrine_Record_Abstract implements Count
             $foreignFieldName = $rel->getForeignFieldName();
             $foreignFieldDef  = $rel->getAssociationTable()->getColumnDefinition($foreignFieldName);
             if ($foreignFieldDef['type'] == 'integer') {
-                foreach ($ids as $i => $ids) {
-                    $ids[$i] = (integer) $ids[$i];
+                foreach ($ids as $i => $id) {
+                    $ids[$i] = (integer) $id;
                 }
             }
             foreach ($ids as $id) {
