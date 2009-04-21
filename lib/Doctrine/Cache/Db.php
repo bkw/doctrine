768af08df0fa58e25b1d08c850df5d318da219be
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2009-04-21 18:15:04 +0000

    [1.0, 1.1] Fixed issue with db cache driver on oracle (closes #2086)
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@5660 625475ce-881a-0410-a577-b389adb331d8

--- lib/Doctrine/Cache/Db.php
+++ lib/Doctrine/Cache/Db.php
@@ -79,19 +79,30 @@ class Doctrine_Cache_Db extends Doctrine_Cache_Driver implements Countable
              . ' WHERE id = ?';
 
         if ($testCacheValidity) {
-            $sql .= ' AND (expire=0 OR expire > ' . time() . ')';
+            $sql .= " AND (expire is null OR expire > '" . date('Y-m-d H:i:s') . "')";
         }
 
-        $result = $this->getConnection()->fetchAssoc($sql, array($id));
+        $result = $this->getConnection()->execute($sql, array($id))->fetchAll(Doctrine::FETCH_NUM);
         
         if ( ! isset($result[0])) {
-            if ($testCacheValidity) {
-                $this->delete($id);
-            }
             return false;
         }
 
-        return unserialize($result[0]['data']);
+        return unserialize($this->_hex2Bin($result[0][0]));
+    }
+
+    protected function _hex2bin($hex)
+    {
+      if ( ! is_string($hex)) {
+          return null;
+      }
+  
+      $bin = '';
+      for ($a = 0; $a < strlen($hex); $a += 2) {
+          $bin .= chr(hexdec($hex{$a} . $hex{($a + 1)}));
+      }
+
+      return $bin;
     }
 
     /**
@@ -102,10 +113,15 @@ class Doctrine_Cache_Db extends Doctrine_Cache_Driver implements Countable
      */
     public function contains($id) 
     {
-        $sql = 'SELECT expire FROM ' . $this->_options['tableName']
-             . ' WHERE id = ? AND (expire=0 OR expire > ' . time() . ')';
+        $sql = 'SELECT id, expire FROM ' . $this->_options['tableName']
+             . ' WHERE id = ?';
 
-        return $this->getConnection()->fetchOne($sql, array($id));
+        $result = $this->getConnection()->fetchOne($sql, array($id));
+
+        if(isset($result[0] )){
+        	return time();
+        }
+        return false;
     }
 
     /**
@@ -120,18 +136,33 @@ class Doctrine_Cache_Db extends Doctrine_Cache_Driver implements Countable
      */
     public function save($id, $data, $lifeTime = false)
     {
-        $sql = 'INSERT INTO ' . $this->_options['tableName']
-             . ' (id, data, expire) VALUES (?, ?, ?)';
+    	if ($this->contains($id)){
+    		//record is in database, do update
+    		$sql = 'UPDATE ' . $this->_options['tableName']
+    			   . ' SET data=?, expire=? '
+             . ' WHERE id = ?';
         
         if ($lifeTime) {
-            $expire = time() + $lifeTime;
+            $expire = date('Y-m-d H:i:s',time() + $lifeTime);
         } else {
-            $expire = 0;
+            $expire = NULL;
         }
-        
-        $params = array($id, serialize($data), $expire);
+        $params = array(bin2hex(serialize($data)), $expire, $id);	
+    	} else {
+    		//record is not in database, do insert
+    		 $sql = 'INSERT INTO ' . $this->_options['tableName']
+             . ' (id, data, expire) VALUES (?, ?, ?)';
 
-        return (bool) $this->getConnection()->exec($sql, $params);
+        if ($lifeTime) {
+            $expire = date('Y-m-d H:i:s', time() + $lifeTime);
+        } else {
+            $expire = NULL;
+        }
+
+        $params = array($id, bin2hex(serialize($data)), $expire);	
+      }
+
+      return (bool) $this->getConnection()->exec($sql, $params);
     }
 
     /**
