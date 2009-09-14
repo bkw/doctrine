a0373afe7691082de15ef55e3cdad25c933ffbde
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2009-09-14 18:27:58 +0000

    [1.0] Fixing sandbox index file
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@6351 625475ce-881a-0410-a577-b389adb331d8

--- tools/sandbox/index.php
+++ tools/sandbox/index.php
@@ -2,11 +2,3 @@
 require_once('config.php');
 
 $models = Doctrine::loadModels('models');
-
-$q = Doctrine_Query::create()
-    ->select('u.id, u.username, p.*')
-    ->from('User u')
-    ->innerJoin('u.Phonenumbers p')
-    ->limit(20);
-
-echo $q->getSql();
