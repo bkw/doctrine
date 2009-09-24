fb545d669bc9f5f1e234fb5fb0e097575357a050
guilhermeblanco <guilhermeblanco@625475ce-881a-0410-a577-b389adb331d8>
2009-09-24 19:25:01 +0000

    [1.0, 1.1, 1.2][DC-59] Fixed issue with mysql attributes. Fixes #2449, the original ticket reference
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@6406 625475ce-881a-0410-a577-b389adb331d8

--- lib/Doctrine/Connection.php
+++ lib/Doctrine/Connection.php
@@ -268,7 +268,7 @@ abstract class Doctrine_Connection extends Doctrine_Configurable implements Coun
             $attribute = $this->getAttributeFromString($attribute);
         }
 
-        if ($attribute >= 100) {
+        if ($attribute >= 100 && $attribute < 1000) {
             if ( ! isset($this->attributes[$attribute])) {
                 return parent::getAttribute($attribute);
             }
@@ -331,7 +331,7 @@ abstract class Doctrine_Connection extends Doctrine_Configurable implements Coun
             $value = parent::getAttributeValueFromString($attributeString, $value);
         }
 
-        if ($attribute >= 100) {
+        if ($attribute >= 100 && $attribute < 1000) {
             parent::setAttribute($attribute, $value);
         } else {
             if ($this->isConnected) {
