66b38dd9810611952b00ed19b99305c18e52c72e
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2009-10-23 18:10:27 +0000

    [1.0][DC-77] Fixed issue with mysql limit subquery not including params
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@6563 625475ce-881a-0410-a577-b389adb331d8

--- lib/Doctrine/Query.php
+++ lib/Doctrine/Query.php
@@ -1150,7 +1150,7 @@ class Doctrine_Query extends Doctrine_Query_Abstract implements Countable, Seria
                     $this->useQueryCache(false);
             
                     // mysql doesn't support LIMIT in subqueries
-                    $list = $this->_conn->execute($subquery, $params)->fetchAll(Doctrine::FETCH_COLUMN);
+                    $list = $this->_conn->execute($subquery, $this->getParams($params))->fetchAll(Doctrine::FETCH_COLUMN);
                     $subquery = implode(', ', array_map(array($this->_conn, 'quote'), $list));
 
                     break;
