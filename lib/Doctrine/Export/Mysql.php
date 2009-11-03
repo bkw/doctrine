8f2de7f4f1f399e001f7a13b8652b0e8c53054e4
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2009-11-03 01:21:08 +0000

    [1.0, 1.1, 1.2][DC-110] Fixes issue with Doctrine_Export_Mysql::createIndexSql()
    git-svn-id: http://svn.doctrine-project.org/branches/1.0@6624 625475ce-881a-0410-a577-b389adb331d8

--- lib/Doctrine/Export/Mysql.php
+++ lib/Doctrine/Export/Mysql.php
@@ -574,6 +574,8 @@ class Doctrine_Export_Mysql extends Doctrine_Export
     public function createIndexSql($table, $name, array $definition)
     {
         $table  = $table;
+        $table  = $this->conn->quoteIdentifier($table, true);
+
         $name   = $this->conn->formatter->getIndexName($name);
         $name   = $this->conn->quoteIdentifier($name);
         $type   = '';
