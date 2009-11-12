2d34065d20150eac923493990f12d3f7fe6e86c8
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2009-11-12 20:47:47 +0000

    [1.2][DC-187] Fix to unique() to allow disabling of unique index and ability to specify options for unique validator
    git-svn-id: http://svn.doctrine-project.org/branches/1.2@6721 625475ce-881a-0410-a577-b389adb331d8

--- tests/Ticket/DC187TestCase.php
+++ tests/Ticket/DC187TestCase.php
@@ -20,7 +20,7 @@
  */
 
 /**
- * Doctrine_Ticket_255_TestCase
+ * Doctrine_Ticket_DC187_TestCase
  *
  * @package     Doctrine
  * @author      Konsta Vesterinen <kvesteri@cc.hut.fi>
@@ -30,27 +30,40 @@
  * @since       1.0
  * @version     $Revision$
  */
-class Doctrine_Ticket_255_TestCase extends Doctrine_UnitTestCase 
+class Doctrine_Ticket_DC187_TestCase extends Doctrine_UnitTestCase 
 {
     public function prepareTables()
     {
-        $this->tables[] = 'Ticket_255_User';
+        $this->tables[] = 'Ticket_DC187_User';
         parent::prepareTables();
     }
 
     public function testTest()
     {
-        Doctrine_Manager::getInstance()->setAttribute(Doctrine_Core::ATTR_VALIDATE, true);
-        $user = new Ticket_255_User();
+        Doctrine_Manager::getInstance()->setAttribute(Doctrine_Core::ATTR_VALIDATE, Doctrine_Core::VALIDATE_ALL);
+
+        $user = new Ticket_DC187_User();
         $user->username = 'jwage';
-        $user->email_address = 'jonwage@gmail.com';
+        $user->email = 'jonwage@gmail.com';
         $user->password = 'changeme';
         $user->save();
+        $user->delete();
 
         try {
-            $user = new Ticket_255_User();
+            $user = new Ticket_DC187_User();
             $user->username = 'jwage';
-            $user->email_address = 'jonwage@gmail.com';
+            $user->email = 'jonwage@gmail.com';
+            $user->password = 'changeme';
+            $user->save();
+            $this->pass();
+        } catch (Exception $e) {
+            $this->fail($e->getMessage());
+        }
+
+        try {
+            $user = new Ticket_DC187_User();
+            $user->username = 'jwage';
+            $user->email = 'jonwage@gmail.com';
             $user->password = 'changeme';
             $user->save();
             $this->fail();
@@ -58,27 +71,27 @@ class Doctrine_Ticket_255_TestCase extends Doctrine_UnitTestCase
             $this->pass();
         }
 
-        Doctrine_Manager::getInstance()->setAttribute(Doctrine_Core::ATTR_VALIDATE, false);
-    }
-
-    public function testTest2()
-    {
-        $dbh = new Doctrine_Adapter_Mock('mysql');
-        $conn = Doctrine_Manager::connection($dbh);
-        $sql = $conn->export->exportClassesSql(array('Ticket_255_User'));
-
-        $this->assertEqual($sql[0], 'CREATE TABLE ticket_255__user (id BIGINT AUTO_INCREMENT, username VARCHAR(255), email_address VARCHAR(255), password VARCHAR(255), UNIQUE INDEX username_email_address_unqidx_idx (username, email_address), PRIMARY KEY(id)) ENGINE = INNODB');
+        Doctrine_Manager::getInstance()->setAttribute(Doctrine_Core::ATTR_VALIDATE, Doctrine_Core::VALIDATE_NONE);
     }
 }
 
-class Ticket_255_User extends Doctrine_Record
+class Ticket_DC187_User extends Doctrine_Record
 {
     public function setTableDefinition()
     {
         $this->hasColumn('username', 'string', 255);
-        $this->hasColumn('email_address', 'string', 255);
+        $this->hasColumn('email', 'string', 255);
         $this->hasColumn('password', 'string', 255);
 
-        $this->unique(array('username', 'email_address'));
+        $this->unique(
+            array('username', 'email'),
+            array('where' => "deleted_at IS NULL"),
+            false
+        );
+    }
+
+    public function setUp()
+    {
+        $this->actAs('SoftDelete');
     }
 }
