8e4074913750dd2e81462e1911e7b2334677e1ab
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2009-12-01 07:06:33 +0000

    [1.0, 1.1] Releasing Doctrine 1.0.14 and 1.1.6
    git-svn-id: http://svn.doctrine-project.org/branches/1.1@6833 625475ce-881a-0410-a577-b389adb331d8

--- lib/Doctrine.php
+++ lib/Doctrine.php
@@ -36,7 +36,7 @@ final class Doctrine
     /**
      * VERSION
      */
-    const VERSION                   = '1.1.5';
+    const VERSION                   = '1.1.6';
 
     /**
      * ERROR CONSTANTS
