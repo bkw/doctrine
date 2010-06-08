4a27f9b64acf77278a5db1be73000fc13aeb796a
jwage <jwage@625475ce-881a-0410-a577-b389adb331d8>
2010-06-08 13:46:33 +0000

    [1.2][DC-712] Fixes issue with pgsql and identifier quoting
    git-svn-id: http://svn.doctrine-project.org/branches/1.2@7632 625475ce-881a-0410-a577-b389adb331d8

--- tools/sandbox/index.php
+++ tools/sandbox/index.php
@@ -3,3 +3,9 @@
 require_once('config.php');
 
 $models = Doctrine_Core::loadModels('models');
+
+$q = Doctrine_Core::getTable('Article')->createQuery();
+
+echo $q->getSqlQuery();
+exit;
+$comments = $q->execute();
